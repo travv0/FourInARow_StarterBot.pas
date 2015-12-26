388801514fbb9c89f9e5f71e5b04796b90d25241
Travis <tssund93@gmail.com>
2015-12-26 13:51:36 -0500

    gitignore

--- four.pas
+++ four.pas
@@ -1,41 +1,41 @@
 program FourInARow;
 
 uses
-    Classes;
+	Classes;
 
 var
-    line, error, cmd: string;
-    parsedLine: TStringList;
+	line, error, cmd: string;
+	parsedLine: TStringList;
 
 begin
-    parsedLine:= TStringList.Create;
-    parsedLine.Delimiter:= #32;
-    error:= '';
+	parsedLine:= TStringList.Create;
+	prsedLine.Delimiter:= #32;
+	error:= '';
 
-    while not eof(input) do begin
-        readln(line);
-        if line = '' then begin
-            error:= error + 'No input found.' + #10;
-            continue
-        end;
+	while not eof(input) do begin
+		readln(line);
+		if line = '' then begin
+			error:= error + 'No input found.' + #10;
+			continue
+		end;
 
-        parsedLine.delimitedText:= line;
+		parsedLine.delimitedText:= line;
 
-        if parsedLine.count = 0 then begin
-            error:= error + 'Unable to parse command.' + #10;
-            continue
-        end;
+		if parsedLine.count = 0 then begin
+			error:= error + 'Unable to parse command.' + #10;
+			continue
+		end;
 
-        cmd:= parsedLine[0];
+		cmd:= parsedLine[0];
 
-        if cmd = 'exit' then
-            break;
+		if cmd = 'exit' then
+			break;
 
-    writeln(line);
+		writeln(line);
 
-    end;
+	end;
 
-    writeln(error);
+	writeln(error);
 
-    readln
+	readln
 end.
